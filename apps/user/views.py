b4487015988eccd7862c911169aa1ae33bf9f3a6
waynecanfly <1370153124@qq.com>
2020-01-11 19:27:59 +0800

    引入django的认证与login模块

--- apps/user/views.py
+++ apps/user/views.py
@@ -2,6 +2,7 @@ import re
 
 from django.http import HttpResponse
 from user.models import User
+from django.contrib.auth import authenticate, login
 from django.shortcuts import render, redirect
 from django.views.generic import View
 from django.conf import settings
@@ -15,6 +16,8 @@ from django.core.mail import send_mail
 
 # /user/register
 from django.urls import reverse
+
+from celery_tasks.tasks import send_register_active_email
 
 
 def register(request):
@@ -74,44 +77,88 @@ class RegisterView(View):
 
         # 发送激活邮件，包含激活链接： 127.0.0.1:8000/user/active/3
         # 使用token对包含用户的身份信息进行加密
-        serializer = Serializer(settings.SECRET_KEY, 3600)
+        serializer = Serializer(settings.SECRET_KEY, expires_in=3600)
         info = {'confirm': user.id}
         token = serializer.dumps(info)
-        token.decode()
+        token = token.decode()
         # 发送激活邮件
         # subject 主题
-        subject = '欢迎信息'
-        message = ''
-        # 发件人
-        sender = settings.EMAIL_HOST_USER
-        receiver = [email]
-        html_message = '<h1>%s, 欢迎您成为注册会员</h>请点击下面的链接激活您的账户</br><a href="http://127.0.0.1:8000/user/' \
-                  'active/%s">http://127.0.0.1:8000/user/active/%s</a>'%(username, token, token)
-        send_mail(subject, message, sender, receiver, html_message=html_message)
-
+        send_register_active_email.delay(email, username, token)
 
         # 返回应答，跳转到首页
         return redirect(reverse('goods:index'))
 
 
 class ActiveView(View):
+    """用户激活"""
+
     def get(self, request, token):
-        # 解密token,获取要激活的用户信息
-        serializer = Serializer(settings.SECRET_KEY, 3600)
+        # token揭密，获取用户信息
+        serializer = Serializer(settings.SECRET_KEY, expires_in=3600)
         try:
+            print(token)
             info = serializer.loads(token)
             user_id = info['confirm']
-            user = User.objects.get(user=user_id)
+
+            # 根据id更改数据库胡is_active
+            user = User.objects.get(id=user_id)
             user.is_active = 1
             user.save()
 
-            # 激活成功，跳转到登陆页面
+            # 跳转登录页面
             return redirect(reverse('user:login'))
+
         except SignatureExpired as e:
-            # 激活链接已经过期
-            return HttpResponse('激活链接已过期')
+            # 激活链接一过期
+            return HttpResponse('激活链接已经过期')
+
 
 # /user/login
 class LoginView(View):
+    """登录页面"""
     def get(self, request):
-        return render(request, 'login.html')
+        """显示登陆页面"""
+        # 判断是否记住用户名
+        if 'username' in request.COOKIES:
+            username = request.COOKIES.get('username')
+            checked = 'checked'
+
+        else:
+            username = ''
+            checked = ''
+
+        return render(request, 'login.html', {'username': username, 'checked': checked})
+
+    def post(self, request):
+        """进行登陆处理"""
+        username = request.POST.get('username')
+        password = request.POST.get('pwd')
+
+        # 数据校验
+        if not all([username, password]):
+            return render(request, 'login.html', {'errmsg': '数据不完整'})
+        # 用户认证
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            if user.is_active:
+                # 用户已激活
+                # 记录用户登陆状态
+                login(request, user)
+                # 跳转到首页
+                response = redirect(reverse('goods:index'))
+                # 判断是否需要记住用户名
+                remember = request.POST.get('remember')
+                
+                if remember == 'on':
+                    # 记住用户名
+                    response.set_cookie('username', username, max_age=7*24*3600)
+                else:
+                    response.delete_cookie('username')
+
+                return response
+            else:
+                # 用户未激活
+                return render(request, 'login.html', {'errmsg': '账户未激活'})
+        else:
+            return render(request, 'login.html', {'errmsg': '用户名或密码错误'})
+
