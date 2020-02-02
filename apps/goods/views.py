38ef12946eee4f33373c9371a4b52c17e3c1618d
waynecanfly <1370153124@qq.com>
2020-02-02 19:53:03 +0800

    商品的详情页视图与模板修改

--- apps/goods/views.py
+++ apps/goods/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
-from goods.models import GoodsType, IndexGoodsBanner, IndexPromotionBanner, IndexTypeGoodsBanner
+from django.shortcuts import render, redirect
+from django.urls import reverse
+from goods.models import GoodsType, IndexGoodsBanner, IndexPromotionBanner, IndexTypeGoodsBanner, GoodsSKU
+from order.models import OrderGoods
 from django.views.generic import View
 from django_redis import get_redis_connection
 from django.core.cache import cache
@@ -7,6 +9,8 @@ from django.core.cache import cache
 
 
 # http://127.0.0.1:8080
+
+
 class IndexView(View):
     """显示首页"""
     def get(self, request):
@@ -56,3 +60,42 @@ class IndexView(View):
             # print(type(context))
         # 使用
         return render(request, 'index.html', context)
+
+
+# /goods/商品id
+class DetailView(View):
+    """详情页"""
+    def get(self, request, goods_id):
+        """显示详情页"""
+        try:
+            sku = GoodsSKU.objects.get(id=goods_id)
+        except GoodsSKU.DoesNotExist:
+            # 商品不存在
+            # 返回首页
+            return redirect(reverse('good:index'))
+
+        # 获取商品的分类信息
+        types = GoodsType.objects.all()
+
+        # 获取商品的评论信息
+        sku_orders = OrderGoods.objects.filter(sku=sku).exclude(comment='') # 移除评论为空的结果
+
+        # 获取新品信息
+        new_skus = GoodsSKU.objects.filter(type=sku.type).order_by('-create_time')  # 新品列表降序
+
+        # 获取用户购物车中商品的数目
+        user = request.user
+        # 购物车数量默认为0
+        cart_count = 0
+        if user.is_authenticated:
+            # 用户已登录
+            conn = get_redis_connection('default')
+            cart_key = 'cart_%d' % user.id
+            cart_count = conn.hlen(cart_key)
+
+        # 组织模板上下文
+        context = {'sku': sku, 'types': types,
+                   'sku_orders': sku_orders, 'new_skus': new_skus,
+                   'cart_count': cart_count}
+        
+        return render(request, 'detail.html', context)
