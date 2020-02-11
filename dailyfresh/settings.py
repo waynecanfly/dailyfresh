a4465aaab95b0fcf325405794fbce621cbce499f
waynecanfly <1370153124@qq.com>
2020-02-11 10:57:26 +0800

    重写

--- dailyfresh/settings.py
+++ dailyfresh/settings.py
@@ -23,7 +23,7 @@ sys.path.insert(0, os.path.join(BASE_DIR, 'apps'))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '*****************************************'
+SECRET_KEY = '*************************************************'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
